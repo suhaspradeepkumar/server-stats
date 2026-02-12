b86a61691a6d76283d54369c91705e18fa8c8bce
Suhas <suhaspradeepkumar@gmail.com>
2026-02-12 22:47:34 +0530

    Add server performance stats script

---
+++ server-stats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "CPU Usage:"
+top -bn1 | grep "Cpu(s)" | awk '{print 100-$8"% used"}'
+
+echo -e "\nMemory Usage:"
+free -h | awk '/Mem:/ {print "Used:", $3, "Free:", $4, "(" $3*100/$2 "% used)"}'
+
+echo -e "\nDisk Usage:"
+df -h / | awk 'NR==2 {print "Used:", $3, "Free:", $4, "(" $5 " used)"}'
+
+echo -e "\nTop 5 CPU Processes:"
+ps -eo pid,comm,%cpu --sort=-%cpu | head -6
+
+echo -e "\nTop 5 Memory Processes:"
+ps -eo pid,comm,%mem --sort=-%mem | head -6
